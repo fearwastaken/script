91e09f9519961a7d66cf4e1fdfc9df5df5c60e4f
fearwastaken <129332557+fearwastaken@users.noreply.github.com>
2023-04-10 18:36:22 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("script.lua", "DarkTheme")
+local Window = Library.CreateLib("zenside.lua", "DarkTheme")
 
 
 local MainTab = Window:NewTab("Dashboard")
