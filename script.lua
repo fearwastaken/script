83b78e67a759cf04b9cae488a4542e0fa78f3414
fearwastaken <129332557+fearwastaken@users.noreply.github.com>
2023-04-11 20:14:48 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,18 +1,11 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("zenside.lua", "DarkTheme")
 
-
-local MainTab = Window:NewTab("Dashboard")
-local MainSection = UniversalTab:NewSection("Dashboard")
 local UniversalTab = Window:NewTab("Universal")
 local UniversalSection = UniversalTab:NewSection("Universal")
 local LocalPlayerTab = Window:NewTab("LocalPlayer")
 local LocalPLRSection = LocalPlayerTab:NewSection("LocalPlayer")
 
---dashboard
-
-MainSection:NewLabel("Welcome! Thanks for using zenside.lua, start going to universal to begin.")
-MainSection:NewLabel("You should also try out the serverside it is great!")
 
 --universal scripts
 
@@ -76,4 +69,108 @@ LocalPLRSection:NewButton("Fly", "vroooooooom", function()
 	loadstring(game:HttpGet(('https://pastebin.com/raw/RkUtdYb0'), true))()
 end)
 
+end
+
+if game.PlaceId == 155615604 then
+local PrisonLifeTab = Window:NewTab("PrisonLife")
+local PrisonLifeSection = PrisonLifeTab:NewSection("PrisonLife")
+
+
+PrisonLifeSection:NewLabel("Trolls")
+
+PrisonLifeSection:NewButton("VehicleFly", "vrooooooooom", function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/VLBwAgwT'),true))()
+end)
+
+PrisonLifeSection:NewLabel("TPS")
+
+PrisonLifeSection:NewButton("Outside of prison", "Teleports You outside of the prison!", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(451.6684265136719, 98.0399169921875, 2216.338134765625)
+            end)
+
+PrisonLifeSection:NewButton("Prison Yard", "Teleports You to the Prison Yard", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(736.4671630859375, 97.99992370605469, 2517.583740234375)
+            end)
+
+PrisonLifeSection:NewButton("Kitchen", "Teleports You to the Kitchen!", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(906.641845703125, 99.98993682861328, 2237.67333984375)
+end)
+
+ PrisonLifeSection:NewButton("Prison Cells", "Teleports You to the Prison Cells!", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(919.5551147460938, 99.98998260498047, 2441.700927734375)
+            end)
+
+ PrisonLifeSection:NewButton("Surveilance Room", "Teleports You to the Surveilance Room!", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(795.251953125, 99.98998260498047, 2327.720703125)
+            end)
+
+ PrisonLifeSection:NewButton("Break Room", "Teleports You to the Break Room!", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(800.0896606445312, 99.98998260498047, 2266.71630859375)
+            end)
+
+PrisonLifeSection:NewButton("Police Armory", "Teleports You to the Police Armory!", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(837.2889404296875, 99.98998260498047, 2270.99658203125)
+            end)
+
+PrisonLifeSection:NewButton("Police Room", "Teleports to to the Police Room", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(836.5386352539062, 99.98998260498047, 2320.604248046875)
+            end)
+
+PrisonLifeSection:NewButton("Cafeteria", "Teleports you to the Cafeteria!", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(918.994873046875, 99.98993682861328, 2325.73095703125)
+			end)
+
+ PrisonLifeSection:NewButton("Criminal Base Inside", "Teleports you to the Criminal Base Inside!", function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-975.8451538085938, 109.32379150390625, 2053.11376953125)
+    end)
+
+PrisonLifeSection:NewLabel("Combat")
+
+PrisonLifeSection:NewButton("Give all weapons", "America!!", function()
+                print("All Weapons Given!")
+                workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["Remington 870"].ITEMPICKUP)
+                workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver.M9.ITEMPICKUP)
+                workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single.Hammer.ITEMPICKUP)
+                workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["AK-47"].ITEMPICKUP)
+        print(currentOption)
+    end)
+
+PrisonLifeSection:NewButton("Remington 870", "yes", function()
+	workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["Remington 870"].ITEMPICKUP)
+end)
+
+PrisonLifeSection:NewButton("M9", "hi", function()
+	workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver.M9.ITEMPICKUP)
+end)
+
+PrisonLifeSection:NewButton("AK-47", "gahagahgahagahag", function()
+	workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["AK-47"].ITEMPICKUP)
+end)
+
+PrisonLifeSection:NewButton("Hammer", "bonk", function()
+	workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single.Hammer.ITEMPICKUP)
+end)
+
+PrisonLifeSection:NewLabel("Misc")
+
+PrisonLifeSection:NewButton("Remove all Doors", "Removes Every Door!", function()
+                for i,d in pairs(game.Workspace.Doors:GetChildren()) do
+                    if d.ClassName == "Model" then
+                        d:Destroy()
+                    end
+                end
+            end)
+
+PrisonLifeSection:NewButton("Remove All Fences", "All Fences Will Be Removed!", function()
+                for i,l in pairs(game.Workspace["Prison_Fences"]:GetChildren()) do
+                    if l.Name == "fence" then
+                        l:Destroy()
+                    end
+                end
+            end)
+
+PrisonLifeSection:NewButton("Remove All Gates", "All Fences Will Be Removed!", function()
+                game.Workspace["Prison_Fences"]["Prison_Gate"]:Destroy()
+                game.Workspace["Prison_Fences"]["gate"]:Destroy()
+            end)
 end
